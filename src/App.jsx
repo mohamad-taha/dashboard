8fffa2efcb0a290d310ee3c3a80ddf16334ac87e
mohamad-taha <mohamadtahakasir@gmail.com>
2025-01-30 00:17:28 +0300

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth/Auth";
 import Signin from "./pages/Auth/Signin";
 import Signup from "./pages/Auth/Signup";
 import ProductsForm from "./pages/Products/ProductsForm";
 import { ContextProvider } from "./context/Context";
+import Cookies from "js-cookie";
 import "./App.css";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!Cookies.get("token")) {
+      navigate("/");
+    }
+  }, []);
   return (
     <>
       <ContextProvider>
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,17 +1,8 @@
-import React, { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
+import React from "react";
+import { Outlet } from "react-router-dom";
 import "./Auth.css";
 
 const Login = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!Cookies.get("token")) {
-      navigate("/");
-    }
-  }, []);
-
   return (
     <div className="register">
       <Outlet />
